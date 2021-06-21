b83b4006e9d30c0e4857a040692e0b56f051c947
fvtrx <ftk2431@gmail.com>
2021-06-21 10:59:01 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { AppContainer, setConfig } from 'react-hot-loader';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+setConfig({ integratedResolver: false });
 
 ReactDOM.render(
-  <React.StrictMode>
+  <AppContainer>
     <App />
-  </React.StrictMode>,
+  </AppContainer>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
