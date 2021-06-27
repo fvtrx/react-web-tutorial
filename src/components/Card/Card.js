503fe7e6c4a95551d21f867490920d77ef976401
fvtrx <ftk2431@gmail.com>
2021-06-27 20:16:48 +0800

    revamp card components

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,33 +4,24 @@ import { Link } from 'react-router-dom';
 function Card(props) {
   return (
     <>
-      <div className="p-10">
-        <Link to={props.path}>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full lazy"
-              src={props.source}
-              alt={props.alternative}
-            />
-            <div class="px-6 py-4"></div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{props.title}</div>
-              <p className="text-gray-700 text-base">{props.description}</p>
-            </div>
-            {/* <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #photography
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #travel
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #winter
-            </span>
-          </div> */}
+      <Link to={props.path}>
+        <div className="rounded-xl overflow-hidden shadow-lg">
+          <img
+            className="w-full h-full lazy"
+            src={props.source}
+            alt={props.alternative}
+          />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{props.title}</div>
+            <p className="text-gray-700 text-base">{props.description}</p>
           </div>
-        </Link>
-      </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              {props.category}
+            </span>
+          </div>
+        </div>
+      </Link>
     </>
   );
 }
